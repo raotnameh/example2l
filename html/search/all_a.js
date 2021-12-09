1c129903a4b4a6ab6fb4fe43c90ef347d9711a36
raotnameh <raotnameh@gmail.com>
2021-12-10 03:17:31 +0530

    added doxygen files

---
+++ html/search/all_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['train_0',['train',['../classann_1_1example2l_1_1Net.html#a4d5f1275339c80cd134e9791b840d6ac',1,'ann::example2l::Net']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "abceflmnrstu",
+  1: "clmnrs",
+  2: "a",
+  3: "beflrstu",
+  4: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Pages"
+};
+
