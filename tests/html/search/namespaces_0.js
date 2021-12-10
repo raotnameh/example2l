d5841cf1621b63486b430c9d723fd3adbd00a6a5
raotnameh <raotnameh@gmail.com>
2021-12-10 18:39:04 +0530

    updated

---
+++ tests/html/search/namespaces_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['activation_0',['activation',['../namespaceann_1_1activation.html',1,'ann']]],
+  ['data_1',['data',['../namespaceann_1_1data.html',1,'ann']]],
+  ['layer_2',['layer',['../namespaceann_1_1layer.html',1,'ann']]],
+  ['loss_3',['loss',['../namespaceann_1_1loss.html',1,'ann']]]
+];
